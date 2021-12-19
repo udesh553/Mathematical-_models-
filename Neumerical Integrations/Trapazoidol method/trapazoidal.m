cc0d41c4cfbe731da0df605a0993910429118289
Udesh Senevirathne <68756214+udesh553@users.noreply.github.com>
2021-12-19 19:47:27 +0530

    Add files via upload
    Numerical Integration

---
+++ Neumerical Integrations/Trapazoidol method/trapazoidal.m
@@ -0,0 +1,10 @@
+ function area = trapazoidal( f, a, b, n )
+ h = (b-a)/(n);               
+x = a:h:b;                     
+y = f(x);                    
+i = 0:n;
+coif = 2*(1).^(i+1);       
+coif(1)=1; coif(end)=1; 
+area = h/2 * sum(coif.*y)
+ endfunction
+ 
